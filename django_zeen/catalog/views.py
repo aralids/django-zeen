928e453d2bea3d9c95a13d5b8ed69753b00edac4
Dilara Sarach <dilara.sarach@genetik.diagnosticum.eu>
2026-04-24 14:02:07 +0200

    homepage, list and detail views added

--- django_zeen/catalog/views.py
+++ django_zeen/catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
 
 def index(request):
     """View function for home page of site."""
@@ -9,17 +11,41 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Chapter 5 challenge
+    num_genres_fiction = Genre.objects.filter(name__icontains=" fiction").count()
+    num_books_fiction = Book.objects.filter(genre__name__icontains=" fiction").count()
+
     context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_genres_fiction": num_genres_fiction,
+        "num_books_fiction": num_books_fiction,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, "catalog/index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Book
